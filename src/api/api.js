36d6a100e6460582be1d24762b172ec97e3a959c
unicornaxe21 <unicornaxe21@gmail.com>
2023-11-22 20:11:11 +0900

    change

--- src/api/api.js
+++ src/api/api.js
@@ -4,6 +4,6 @@ axios.defaults.withCredentials = true;
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
 export default axios.create({
-  baseURL: "https://qr-appraisal.com.au/api"
+  baseURL: "https://qr-appraisal.com.au/apiii"
   // baseURL: "http://localhost:3000/api",
 });
